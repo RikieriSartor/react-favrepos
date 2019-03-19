f467511ede44752bb698f126487d6b9035bb84ae
Rikieri Sartor <riki_eri@hotmail.com>
2019-03-18 21:52:52 -0300

    Adicionando Reactotron

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
+import './config/reactotron';
 import store from './store';
+
 import TodoList from './TodoList';
 
 const App = () => (
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
+
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const tronMiddleware = process.env.NODE_ENV === 'development' ? console.tron.createEnhancer : () => {};
+
+const store = createStore(reducers, compose(tronMiddleware()));
 
 export default store;
