60e1797ec0f567b0cb0f6626fa79ca33e564db92
Rikieri Sartor <riki_eri@hotmail.com>
2019-03-26 22:09:45 -0300

    First commit

---
+++ src/pages/main/index.js
@@ -0,0 +1,32 @@
+import React, { Component, Fragment } from 'react';
+
+export default class Main extends Component {
+  state = {
+    repositoryInput: '',
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <form onSubmit={this.handleAddRepository}>
+          <input
+            placeholder="usuário/repositório"
+            value={this.state.repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">Adicionar</button>
+        </form>
+
+        <ul>
+          <li>
+            <p>
+              <strong>facebook/react</strong> A declarative, efficient, and flexible JavaScript library for building user interfaces.
+            </p>
+            <a href="#">Acessar</a>
+          </li>
+        </ul>
+
+      </Fragment>
+    );
+  }
+}
